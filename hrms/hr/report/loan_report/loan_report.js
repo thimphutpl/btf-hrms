0b36cfa5dc566c464b789b2b9f4dde50ef9d7f5e
Tulasi-111 <tulasi.devi@thimphutechpark.bt>
2026-08-13 11:44:58 +0600

    travel role 13-08-26

--- hrms/hr/report/loan_report/loan_report.js
+++ hrms/hr/report/loan_report/loan_report.js
@@ -1,46 +1,50 @@
-// Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
+// Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
+/* eslint-disable */
 
 frappe.query_reports["Loan Report"] = {
 	"filters": [
-		{
-			"fieldname": "fiscal_year",
-			"label": __("Fiscal Year"),
-			"fieldtype": "Link",
-			"options": "Fiscal Year",
-			"default": frappe.sys_defaults.fiscal_year,
-		},
-		{
-			"fieldname": "month",
-			"label": __("Month"),
-			"fieldtype": "Select",
-			"options": "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec",
-			"default": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][frappe.datetime.str_to_obj(frappe.datetime.get_today()).getMonth()],
-		},
-		{
-			"fieldname": "cost_center",
-			"label": __("Cost Center"),
-			"fieldtype": "Link",
-			"options": "Cost Center",
-		},
-		{
-			"fieldname": "employee",
-			"label": __("Employee"),
-			"fieldtype": "Link",
-			"options": "Employee"
-		},
-		{
-			"fieldname": "bank",
-			"label": __("Bank"),
-			"fieldtype": "Link",
-			"options": "Financial Institution"
-		},
-		{
-			"fieldname": "company",
-			"label": __("Company"),
-			"fieldtype": "Link",
-			"options": "Company",
-			"default": frappe.defaults.get_user_default("Company"),
-		},
-	]
+	{
+		"fieldname": "fiscal_year",
+		"label": "Fiscal Year",
+		"fieldtype": "Link",
+		"options": "Fiscal Year",
+		"reqd": 1,
+				"default": function() {
+					// Get current fiscal year based on today's date
+					var today = frappe.datetime.get_today();
+					var fiscal_year = frappe.sys_defaults.fiscal_year;
+					
+					// Or use user's default fiscal year
+					var user_fiscal_year = frappe.defaults.get_user_default("fiscal_year");
+					return user_fiscal_year || fiscal_year;
+				}()
+
+	},
+	{
+				"fieldname": "month",
+				"label": "Month",
+				"fieldtype": "Select",
+				"options": "\nJanuary\nFebruary\nMarch\nApril\nMay\nJune\nJuly\nAugust\nSeptember\nOctober\nNovember\nDecember",
+				"default": ""
+			},
+	{
+		"fieldname": "employee",
+		"label": "Employee",
+		"fieldtype": "Link",
+		"options": "Employee"
+	},
+	{
+		"fieldname": "employee_name",
+		"label": "Employee Name",
+		"fieldtype": "Data"
+	},
+	{
+		"fieldname": "company",
+		"label": "Company",
+		"fieldtype": "Link",
+		"options": "Company",
+		
+	}
+]
 };
--- hrms/hr/report/loan_report/loan_report.py
+++ hrms/hr/report/loan_report/loan_report.py
@@ -1,78 +1,134 @@
-# Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
-# For license information, please see license.txt
-
+from __future__ import unicode_literals
 import frappe
 from frappe import _
 
-
 def execute(filters=None):
-	if not filters:
-		filters = {}
-	columns, data = [], []
-	data = get_data(filters)
-	if not data:
-		return columns, data
-	columns = get_columns(data)
-	return columns, data
+    columns = get_columns()
+    data = get_data(filters)
+    return columns, data
 
-def get_columns(data):
-	columns = [
-		_("Employee") + ":Link/Employee:80", 
-		_("Employee Name") + ":Data:140", 
-		_("CID") + ":Data:120", 
-		_("Designation") + ":Link/Designation:120",
-		_("Loan Type") + ":Data:140", 
-		_("Loan From") + ":Data:160", 
-		_("Account No") + ":Data:140",  
-		_("Deduction Amount") + ":Currency:140", 
-		_("Total Deductible Amount") + ":Currency:170", 
-		_("Balance Amount") + ":Currency:140",
-		_("Company") + ":Link/Company:120", 
-		_("Cost Center") + ":Link/Cost Center:120", 
-		_("Branch") + ":Link/Branch:120", 
-		_("Department") + ":Link/Department:120",
-		_("Division") + ":Link/Department:120", 
-		_("Section") + ":Link/Department:120", 
-		_("Year") + ":Data:80", 
-		_("Month") + ":Data:80"
-	]
-	return columns
+def get_columns():
+    return [
+        {
+            "fieldname": "employee",
+            "label": _("Employee"),
+            "fieldtype": "Link",
+            "options": "Employee",
+            "width": 120
+        },
+        {
+            "fieldname": "employee_name",
+            "label": _("Employee Name"),
+            "fieldtype": "Data",
+            "width": 180
+        },
+        {
+            "fieldname": "month",
+            "label": _("Month"),
+            "fieldtype": "Data",
+            "width": 120
+        },
+        {
+            "fieldname": "gross_pay",
+            "label": _("Gross Pay"),
+            "fieldtype": "Currency",
+            "width": 140,
+            "options": "currency"
+        },
+        {
+            "fieldname": "financial_loan_deduction",
+            "label": _("Total Loan Deduction"),
+            "fieldtype": "Currency",
+            "width": 180,
+            "options": "currency"
+        },
+        {
+            "fieldname": "net_pay",
+            "label": _("Net Pay"),
+            "fieldtype": "Currency",
+            "width": 140,
+            "options": "currency"
+        },
+        {
+            "fieldname": "posting_date",
+            "label": _("Posting Date"),
+            "fieldtype": "Date",
+            "width": 120
+        }
+    ]
 
-def get_data(filters):
-	conditions, filters = get_conditions(filters)
-
-	data = frappe.db.sql("""
-		select t1.employee, t3.employee_name, t3.passport_number, t1.designation,
-			t2.reference_type, t2.institution_name, t2.reference_number, t2.amount, t2.total_deductible_amount, t2.total_outstanding_amount,
-			t1.company, t1.cost_center, t1.branch, t1.department, t1.division, t1.section,
-			t1.fiscal_year, t1.month
-		from `tabSalary Slip` t1, `tabSalary Detail` t2, `tabEmployee` t3
-		where t1.docstatus = 1 {}
-		and t3.employee = t1.employee
-		and t2.parent = t1.name
-		and t2.parentfield = 'deductions'
-		and case when t2.institution_name = 'RICBL' then t2.reference_type like '%loan%' else 1 = 1 end
-		and exists
-			(select 1
-				from `tabSalary Component` sc
-				where sc.name = t2.salary_component
-			)
-		and t2.reference_type != 'NULL'
-	""".format(conditions))
-	return data
-
-def get_conditions(filters):
-	conditions = ""
-	if filters.get("month"):
-		month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", 
-			"Dec"].index(filters["month"]) + 1
-		filters["month"] = month
-		conditions += " and t1.month = {}".format(filters.get("month"))
-	
-	if filters.get("fiscal_year"): conditions += " and t1.fiscal_year = {}".format(filters.get("fiscal_year"))
-	if filters.get("company"): conditions += " and t1.company = '{}'".format(filters.get("company"))
-	if filters.get("employee"): conditions += " and t1.employee = '{}'".format(filters.get("employee"))
-	if filters.get("bank"): conditions += "and t2.institution_name = '{0}'".format(filters.bank)
-	if filters.get("cost_center"): conditions += " and exists(select 1 from `tabCost Center` cc where t1.cost_center = cc.name and (cc.parent_cost_center = '{0}' or cc.name = '{0}'))".format(filters.cost_center)
-
-	return conditions, filters
+def get_data(filters=None):
+    if not filters:
+        filters = {}
+    
+    # Build filter conditions
+    conditions = ["sl.docstatus = 1"]
+    params = []
+    
+    # Fiscal Year filter
+    if filters.get("fiscal_year"):
+        try:
+            fiscal_year = frappe.get_doc("Fiscal Year", filters.get("fiscal_year"))
+            conditions.append("sl.posting_date BETWEEN %s AND %s")
+            params.extend([fiscal_year.year_start_date, fiscal_year.year_end_date])
+        except:
+            pass
+    
+    # Month filter
+    if filters.get("month"):
+        conditions.append("sl.month = %s")
+        params.append(filters.get("month"))
+    
+    # Employee filters
+    if filters.get("employee"):
+        conditions.append("sl.employee = %s")
+        params.append(filters.get("employee"))
+    
+    if filters.get("employee_name"):
+        conditions.append("sl.employee_name LIKE %s")
+        params.append("%" + filters.get("employee_name") + "%")
+    
+    # Company filter
+    if filters.get("company"):
+        conditions.append("sl.company = %s")
+        params.append(filters.get("company"))
+    
+    # Department filter
+    if filters.get("department"):
+        conditions.append("sl.department = %s")
+        params.append(filters.get("department"))
+    
+    # Branch filter
+    if filters.get("branch"):
+        conditions.append("sl.branch = %s")
+        params.append(filters.get("branch"))
+    
+    # Build optimized query with subquery for Financial Institution Loan components
+    query = """
+        SELECT 
+            sl.employee,
+            sl.employee_name,
+            sl.month,
+            sl.gross_pay,
+            sl.total_deduction,
+            sl.net_pay,
+            sl.posting_date,
+            (
+                SELECT COALESCE(SUM(sd.amount), 0)
+                FROM `tabSalary Detail` sd
+                WHERE sd.parent = sl.name
+                AND sd.salary_component IN (
+                    SELECT sc.name 
+                    FROM `tabSalary Component` sc
+                    WHERE sc.salary_component = 'Financial Institution Loan'
+                )
+            ) as financial_loan_deduction
+        FROM `tabSalary Slip` sl
+        WHERE {conditions}
+        ORDER BY sl.posting_date DESC, sl.employee
+    """.format(conditions=" AND ".join(conditions))
+    
+    # Execute query
+    data = frappe.db.sql(query, tuple(params), as_dict=1)
+    
+    return data
